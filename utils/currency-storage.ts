0ed616fad6e1eaee21b0d8d7d4fb1328bee90cbf
Alexander Bolotskov <r3disrussian@gmail.com>
2021-08-08 03:56:14 +0800

    feat: 🎸 adds api response mock

--- utils/currency-storage.ts
+++ utils/currency-storage.ts
@@ -12,17 +12,46 @@ export class CurrencyStorage {
   }
 
   async fetch(): Promise<CurrencySheet> {
-    const result = await fetch(`https://freecurrencyapi.net/api/v1/rates?base_currency=USD`, {
+    if (process.env.DEV_MODE === '1') {
+      return this._getMockSheet();
+    }
+    const sheet = await this._getSheetFromApi();
+    const [currencies] = Object.values(sheet.data)
+    CurrencyStorage._dict = { ...currencies }
+    return currencies
+  }
+
+  private async _getSheetFromApi(): Promise<ICurrencyAPIResponse> {
+    return fetch(`https://freecurrencyapi.net/api/v1/rates?base_currency=USD`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'apikey': process.env.CURRENCY_API_KEY,
         'Content-Type': 'application/json'
       }
-    });
-    const sheet: ICurrencyAPIResponse = await result.json()
-    const [currencies] = Object.values(sheet.data)
-    CurrencyStorage._dict = { ...currencies }
-    return currencies
+    }).then(r => {
+      if (r.status === 200) {
+        return r.json();
+      } else {
+        throw new APIError(r);
+      }
+    })
+  }
+
+  private _getMockSheet(): CurrencySheet {
+    return {
+      CAD: 1.25,
+      EUR: 0.85,
+      JPY: 110.27,
+      RUB: 73.44,
+      USD: 1,
+    }
+  }
+}
+
+class APIError extends Error {
+  constructor(response: Response) {
+    super()
+    this.message = `HTTP ${response.status}: ${response.status} at ${response.url}`;
   }
 }
