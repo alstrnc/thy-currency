b64a6209bdd0dd915033a6746bf015dc32f4f19e
Alexander Bolotskov <r3disrussian@gmail.com>
2021-08-08 03:35:28 +0800

    feat: 🎸 updates currencies sheet every 12 hours

--- pages/api/currencies.ts
+++ pages/api/currencies.ts
@@ -1,18 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import dotenv from 'dotenv'
-import { CurrencyStorage } from '../../utils/currency-storage';
+import { CurrencyStorage } from '../../utils/currency-storage'
+import differenceInHours from 'date-fns/differenceInHours'
 
 dotenv.config()
 
 const currencyStorage = new CurrencyStorage()
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let currencies = currencyStorage.getAll();
-  if (!currencies) {
-    currencies = await currencyStorage.fetch()
-    console.debug('Got currencies from outer API')
+let lastUpdated: Date
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  let currencies = currencyStorage.getAll()
+  const canObtainLocally = !!lastUpdated && differenceInHours(new Date(), lastUpdated) < 12
+  if (currencies && canObtainLocally) {
+    console.debug(`Got currencies locally (from ${lastUpdated.toISOString()})`)
   } else {
-    console.debug('Got currencies locally')
+    currencies = await currencyStorage.fetch()
+    lastUpdated = new Date()
+    console.debug('Got currencies from outer API')
   }
   return res.status(200).json(currencies)
 }
